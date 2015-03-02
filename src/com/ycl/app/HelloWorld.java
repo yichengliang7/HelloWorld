0cd76e4188293ce4cb7555cfd97755b57c8fd73c
Yichengliang7 <Yichengliang7@gmail.com>
2015-03-02 21:31:03 +0800

    First Commit ...

---
+++ src/com/ycl/app/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.ycl.app;
+
+/**
+ * 新手类
+ * Created by yichengliang on 15/3/2.
+ */
+public class HelloWorld {
+    /**
+     * 主函数
+     * @param args 参数
+     */
+    public static void main(String[] args){
+        System.out.println("Hello World !");
+    }
+}
